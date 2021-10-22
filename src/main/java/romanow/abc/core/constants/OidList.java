8e0643f90a477cb1cb0b0ab85a899e8f59f96b1e
romanow-nsk <romanow@ngs.ru>
2021-10-23 01:34:28 +0700

    API сервера - обработка измерений

---
+++ src/main/java/romanow/abc/core/constants/OidList.java
@@ -0,0 +1,9 @@
+package romanow.abc.core.constants;
+
+import romanow.abc.core.mongo.DAO;
+
+import java.util.ArrayList;
+
+public class OidList extends DAO {
+    public final ArrayList<Long> oids = new ArrayList<>();
+}
